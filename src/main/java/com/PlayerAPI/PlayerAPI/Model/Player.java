a1e56b8241031450f6d9496090b5ae5670e9c24a
Lamia <lama5alomairi@gmail.com>
2023-07-25 16:15:54 +0400

    Add attributes into Player class

--- src/main/java/com/PlayerAPI/PlayerAPI/Model/Player.java
+++ src/main/java/com/PlayerAPI/PlayerAPI/Model/Player.java
@@ -1,4 +1,22 @@
 package com.PlayerAPI.PlayerAPI.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+@Data
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    public String name;
+    public String email;
 }
