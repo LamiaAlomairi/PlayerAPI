e9a5e192662b82f8a720f7eefbf246328c7851da
Lamia <lama5alomairi@gmail.com>
2023-07-25 16:20:55 +0400

    create addPlayer method in PlayerService

--- src/main/java/com/PlayerAPI/PlayerAPI/Service/PlayerService.java
+++ src/main/java/com/PlayerAPI/PlayerAPI/Service/PlayerService.java
@@ -1,4 +1,17 @@
 package com.PlayerAPI.PlayerAPI.Service;
 
+import com.PlayerAPI.PlayerAPI.Model.Player;
+import com.PlayerAPI.PlayerAPI.Repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlayerService {
+    @Autowired
+    PlayerRepository playerRepository;
+
+    /*****  Add Player *****/
+    public void addPlayer(Player player){
+        playerRepository.save(player);
+    }
 }
