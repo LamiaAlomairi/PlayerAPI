e1442686d7ace63b13a0d600b77de584acc5cfe3
Lamia <lama5alomairi@gmail.com>
2023-07-25 16:22:27 +0400

    Create addPlayer methon in PlayerController

--- src/main/java/com/PlayerAPI/PlayerAPI/Controller/PlayerController.java
+++ src/main/java/com/PlayerAPI/PlayerAPI/Controller/PlayerController.java
@@ -1,4 +1,20 @@
 package com.PlayerAPI.PlayerAPI.Controller;
 
+import com.PlayerAPI.PlayerAPI.Model.Player;
+import com.PlayerAPI.PlayerAPI.Service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/player")
+@CrossOrigin("*")
 public class PlayerController {
+    @Autowired
+    PlayerService playerService;
+
+    /*****  Add Player *****/
+    @PostMapping
+    public void addPlayer(@RequestBody Player player) {
+        playerService.addPlayer(player);
+    }
 }
